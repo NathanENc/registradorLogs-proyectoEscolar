a14de34b9138605e01424a8b71d806b32dc14938
erikgm02 <erikgm02@hotmail.com>
2025-10-26 23:19:39 -0600

    Update LogSummary.java
    Esta clase es un contenedor de datos o "molde". Su único trabajo es guardar los resultados del análisis de un solo archivo de log.

--- LogSummary.java
+++ LogSummary.java
@@ -1,51 +1,42 @@
+import java.util.HashMap;
 import java.util.Map;
+
 
 public class LogSummary {
 
+   
     private String nombreArchivo;
     private int totalCoincidencias;
+    
+  
     private Map<String, Integer> coincidenciasPorPatron;
-    private Map<String, Integer> reportePorFecha;
-    private boolean esCorrupto;
 
-    public LogSummary(String nombre) {
-        this.nombreArchivo = nombre;
+   
+    public LogSummary(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+        this.totalCoincidencias = 0;
+        this.coincidenciasPorPatron = new HashMap<String, Integer>();
     }
 
-    public void incrementarCoincidencia(String patron, int cantidad) { }
+   
+    public void registrarCoincidencia(String patron, int n) {
+       
+        this.totalCoincidencias = this.totalCoincidencias + n;
+        
+      
+        int conteoAnterior = this.coincidenciasPorPatron.getOrDefault(patron, 0);
+        this.coincidenciasPorPatron.put(patron, conteoAnterior + n);
+    }
 
     public String getNombreArchivo() {
         return nombreArchivo;
-    }
-    public void setNombreArchivo(String nombreArchivo) {
-        this.nombreArchivo = nombreArchivo;
     }
 
     public int getTotalCoincidencias() {
         return totalCoincidencias;
     }
-    public void setTotalCoincidencias(int totalCoincidencias) {
-        this.totalCoincidencias = totalCoincidencias;
-    }
 
     public Map<String, Integer> getCoincidenciasPorPatron() {
         return coincidenciasPorPatron;
-    }
-    public void setCoincidenciasPorPatron(Map<String, Integer> coincidenciasPorPatron) {
-        this.coincidenciasPorPatron = coincidenciasPorPatron;
-    }
-
-    public Map<String, Integer> getReportePorFecha() {
-        return reportePorFecha;
-    }
-    public void setReportePorFecha(Map<String, Integer> reportePorFecha) {
-        this.reportePorFecha = reportePorFecha;
-    }
-
-    public boolean isEsCorrupto() {
-        return esCorrupto;
-    }
-    public void setEsCorrupto(boolean esCorrupto) {
-        this.esCorrupto = esCorrupto;
     }
 }
